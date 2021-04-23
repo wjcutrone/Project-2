4e24fd256497e0578a322c2ef1ab2a9b675e5704
wjcutrone <73838888+wjcutrone@users.noreply.github.com>
2021-04-22 21:24:08 -0400

    updates

--- App/app.py
+++ App/app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, Response, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/marker-cluster")
+def marker_cluster():
+    return render_template("marker-cluster.html")
+
+@app.route("/choropleth-time")
+def choropleth_time():
+    return render_template("choropleth-time.html")
+
+@app.route("/source-data")
+def source_data():
+    return render_template("data.html")
+
+@app.route("/data")
+def get_csv():
+    with open("./static/data/new_data.csv") as f:
+        csv = f.read()
+
+    return Response(csv, mimetype="text/csv")
+
+@app.route("/marker")
+def send_js():
+    return app.send_static_file("marker.js")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
