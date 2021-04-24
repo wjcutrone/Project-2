16d45795282a3a09681e0064d4487947d9efe1cf
gonzal29 <75150803+gonzal29@users.noreply.github.com>
2021-04-24 08:55:32 -0400

    Add files via upload

---
+++ Temperature/plots.js
@@ -0,0 +1,333 @@
+var trace1 = {
+  y: Wind_Speed.Al,
+  name: "AL",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace2 = {
+  y: Wind_Speed.AR,
+  name: "AR",
+  type: "box",
+  boxpoints: "all"
+};
+var trace3 = {
+  y: Wind_Speed.AZ,
+  name: "AZ",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace4 = {
+  y: Wind_Speed.CA,
+  name: "CA",
+  type: "box",
+  boxpoints: "all"
+};
+var trace5 = {
+  y: Wind_Speed.CO,
+  name: "CO",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace6 = {
+  y: Wind_Speed.CT,
+  name: "CT",
+  type: "box",
+  boxpoints: "all"
+};
+var trace7 = {
+  y: Wind_Speed.DC,
+  name: "DC",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace8 = {
+  y: Wind_Speed.DE,
+  name: "DE",
+  type: "box",
+  boxpoints: "all"
+};
+var trace9 = {
+  y: Wind_Speed.FL,
+  name: "FL",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace10 = {
+  y: Wind_Speed.GA,
+  name: "GA",
+  type: "box",
+  boxpoints: "all"
+};
+var trace11 = {
+  y: Wind_Speed.IA,
+  name: "IA",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace12 = {
+  y: Wind_Speed.ID,
+  name: "ID",
+  type: "box",
+  boxpoints: "all"
+};
+var trace13 = {
+  y: Wind_Speed.IL,
+  name: "IL",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace14 = {
+  y: Wind_Speed.IN,
+  name: "IN",
+  type: "box",
+  boxpoints: "all"
+};
+var trace15 = {
+  y: Wind_Speed.KS,
+  name: "KS",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace16 = {
+  y: Wind_Speed.KY,
+  name: "KY",
+  type: "box",
+  boxpoints: "all"
+};
+var trace17 = {
+  y: Wind_Speed.LA,
+  name: "LA",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace18 = {
+  y: Wind_Speed.MA,
+  name: "MA",
+  type: "box",
+  boxpoints: "all"
+};
+var trace19 = {
+  y: Wind_Speed.MD,
+  name: "MD",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace20 = {
+  y: Wind_Speed.ME,
+  name: "ME",
+  type: "box",
+  boxpoints: "all"
+};
+var trace21 = {
+  y: Wind_Speed.MI,
+  name: "MI",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace22 = {
+  y: Wind_Speed.MN,
+  name: "MN",
+  type: "box",
+  boxpoints: "all"
+};
+var trace23 = {
+  y: Wind_Speed.MO,
+  name: "MO",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace24 = {
+  y: Wind_Speed.MS,
+  name: "MS",
+  type: "box",
+  boxpoints: "all"
+};
+var trace25 = {
+  y: Wind_Speed.MT,
+  name: "MT",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace26 = {
+  y: Wind_Speed.NC,
+  name: "NC",
+  type: "box",
+  boxpoints: "all"
+};
+var trace27 = {
+  y: Wind_Speed.ND,
+  name: "ND",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace28 = {
+  y: Wind_Speed.NE,
+  name: "NE",
+  type: "box",
+  boxpoints: "all"
+};
+var trace29 = {
+  y: Wind_Speed.NH,
+  name: "NH",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace30 = {
+  y: Wind_Speed.NJ,
+  name: "NJ",
+  type: "box",
+  boxpoints: "all"
+};
+var trace31 = {
+  y: Wind_Speed.NM,
+  name: "NM",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace32 = {
+  y: Wind_Speed.NV,
+  name: "NV",
+  type: "box",
+  boxpoints: "all"
+};
+var trace33 = {
+  y: Wind_Speed.NY,
+  name: "NY",
+  type: "box",
+  boxpoints: "all"
+};
+var trace34 = {
+  y: Wind_Speed.OH,
+  name: "OH",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace35 = {
+  y: Wind_Speed.OK,
+  name: "OK",
+  type: "box",
+  boxpoints: "all"
+};
+var trace36 = {
+  y: Wind_Speed.OR,
+  name: "OR",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace37 = {
+  y: Wind_Speed.PA,
+  name: "PA",
+  type: "box",
+  boxpoints: "all"
+};
+var trace38 = {
+  y: Wind_Speed.RI,
+  name: "RI",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace39 = {
+  y: Wind_Speed.SC,
+  name: "SC",
+  type: "box",
+  boxpoints: "all"
+};
+var trace40 = {
+  y: Wind_Speed.SD,
+  name: "SD",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace41 = {
+  y: Wind_Speed.TN,
+  name: "TN",
+  type: "box",
+  boxpoints: "all"
+};
+var trace42 = {
+  y: Wind_Speed.TX,
+  name: "TX",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace43 = {
+  y: Wind_Speed.UT,
+  name: "UT",
+  type: "box",
+  boxpoints: "all"
+};
+var trace44 = {
+  y: Wind_Speed.VA,
+  name: "VA",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace45 = {
+  y: Wind_Speed.VT,
+  name: "VT",
+  type: "box",
+  boxpoints: "all"
+};
+var trace46 = {
+  y: Wind_Speed.WA,
+  name: "WA",
+  type: "box",
+  boxpoints: "all"
+};
+
+var trace47 = {
+  y: Wind_Speed.WI,
+  name: "WI",
+  type: "box",
+  boxpoints: "all"
+};
+var trace48 = {
+  y: Wind_Speed.WV,
+  name: "WV",
+  type: "box",
+  boxpoints: "all"
+};
+
+var data = [trace1,trace2,trace3,trace4,
+  trace5,trace6,trace7,trace8,
+  trace9,trace10,trace11,trace12,
+  trace13,trace14,trace15,trace16,
+  trace17,trace18,trace19,trace20,
+  trace21,trace22,trace23,trace24,
+  trace25,trace26,trace27,trace28,
+  trace29,trace30,trace31,trace32,
+  trace33,trace34,trace35,trace36,
+  trace37,trace38,trace39,trace40,
+  trace41,trace42,trace43,trace44,
+  trace45,trace46,trace47,trace48,
+];
+
+var layout = {
+  title: "Wind Speed Across States, 2019-2020",
+  yaxis: { title: "Wind Speed(mph)"}
+};
+
+// Plot the chart to a div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
