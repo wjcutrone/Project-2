582b03c3b055d1db46bd793982acda1fec750f88
gonzal29 <75150803+gonzal29@users.noreply.github.com>
2021-04-24 08:48:25 -0400

    Add files via upload

---
+++ Cartogram/plots3.js
@@ -0,0 +1,333 @@
+var trace1 = {
+    y: Temp.Al,
+    name: "AL",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace2 = {
+    y: Temp.AR,
+    name: "AR",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace3 = {
+    y: Temp.AZ,
+    name: "AZ",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace4 = {
+    y: Temp.CA,
+    name: "CA",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace5 = {
+    y: Temp.CO,
+    name: "CO",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace6 = {
+    y: Temp.CT,
+    name: "CT",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace7 = {
+    y: Temp.DC,
+    name: "DC",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace8 = {
+    y: Temp.DE,
+    name: "DE",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace9 = {
+    y: Temp.FL,
+    name: "FL",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace10 = {
+    y: Temp.GA,
+    name: "GA",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace11 = {
+    y: Temp.IA,
+    name: "IA",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace12 = {
+    y: Temp.ID,
+    name: "ID",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace13 = {
+    y: Temp.IL,
+    name: "IL",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace14 = {
+    y: Temp.IN,
+    name: "IN",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace15 = {
+    y: Temp.KS,
+    name: "KS",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace16 = {
+    y: Temp.KY,
+    name: "KY",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace17 = {
+    y: Temp.LA,
+    name: "LA",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace18 = {
+    y: Temp.MA,
+    name: "MA",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace19 = {
+    y: Temp.MD,
+    name: "MD",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace20 = {
+    y: Temp.ME,
+    name: "ME",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace21 = {
+    y: Temp.MI,
+    name: "MI",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace22 = {
+    y: Temp.MN,
+    name: "MN",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace23 = {
+    y: Temp.MO,
+    name: "MO",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace24 = {
+    y: Temp.MS,
+    name: "MS",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace25 = {
+    y: Temp.MT,
+    name: "MT",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace26 = {
+    y: Temp.NC,
+    name: "NC",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace27 = {
+    y: Temp.ND,
+    name: "ND",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace28 = {
+    y: Temp.NE,
+    name: "NE",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace29 = {
+    y: Temp.NH,
+    name: "NH",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace30 = {
+    y: Temp.NJ,
+    name: "NJ",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace31 = {
+    y: Temp.NM,
+    name: "NM",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace32 = {
+    y: Temp.NV,
+    name: "NV",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace33 = {
+    y: Temp.NY,
+    name: "NY",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace34 = {
+    y: Temp.OH,
+    name: "OH",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace35 = {
+    y: Temp.OK,
+    name: "OK",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace36 = {
+    y: Temp.OR,
+    name: "OR",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace37 = {
+    y: Temp.PA,
+    name: "PA",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace38 = {
+    y: Temp.RI,
+    name: "RI",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace39 = {
+    y: Temp.SC,
+    name: "SC",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace40 = {
+    y: Temp.SD,
+    name: "SD",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace41 = {
+    y: Temp.TN,
+    name: "TN",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace42 = {
+    y: Temp.TX,
+    name: "TX",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace43 = {
+    y: Temp.UT,
+    name: "UT",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace44 = {
+    y: Temp.VA,
+    name: "VA",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace45 = {
+    y: Temp.VT,
+    name: "VT",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace46 = {
+    y: Temp.WA,
+    name: "WA",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var trace47 = {
+    y: Temp.WI,
+    name: "WI",
+    type: "box",
+    boxpoints: "all"
+  };
+  var trace48 = {
+    y: Temp.WV,
+    name: "WV",
+    type: "box",
+    boxpoints: "all"
+  };
+  
+  var data = [trace1, trace2,trace3,trace4,
+    trace5,trace6,trace7,trace8,
+    trace9,trace10,trace11,trace12,
+    trace13,trace14,trace15,trace16,
+    trace17,trace18,trace19,trace20,
+    trace21,trace22,trace23,trace24,
+    trace25,trace26,trace27,trace28,
+    trace29,trace30,trace31,trace32,
+    trace33,trace34,trace35,trace36,
+    trace37,trace38,trace39,trace40,
+    trace41,trace42,trace43,trace44,
+    trace45,trace46,trace47,trace48]; 
+  
+  var layout = {
+    title: "Temperature Across States, 2019-2020",
+    yaxis: { title: "Temperature(F)"}
+  };
+  
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("plot", data, layout);
+  
